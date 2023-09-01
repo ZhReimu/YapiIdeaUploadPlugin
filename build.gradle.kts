056715e0a54b6d7992a46ff42d0c9244dc4b9543
博麗靈夢 <1725338233@qq.com>
2023-09-01 19:38:30 +0800

    移除多余配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,10 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.0"
     id("org.jetbrains.intellij") version "1.15.0"
-}
-
-buildscript {
-    repositories {
-        mavenLocal()
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
-        mavenCentral()
-        maven { url = uri("https://plugins.gradle.org/m2/") }
-        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
-        maven { url = uri("https://dl.bintray.com/jetbrains/intellij-plugin-service") }
-        maven { url = uri("https://dl.bintray.com/jetbrains/intellij-third-party-dependencies/") }
-    }
-    dependencies {
-        classpath("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.15.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
 }
 
 group = "com.dbb"
 version = "1.8.2"
-
 
 repositories {
     mavenLocal()
@@ -46,11 +28,8 @@ intellij {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
     }
     patchPluginXml {
         sinceBuild.set("202")
