7c79dc757e0f2c6fba18f25f71f23113bd57d902
博麗霊夢 <1725338233@qq.com>
2023-09-01 16:25:45 +0800

    迁移构建脚本到 kts, 增加默认图标

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.15.0"
+}
+
+buildscript {
+    repositories {
+        mavenLocal()
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
+        maven { url = uri("https://dl.bintray.com/jetbrains/intellij-plugin-service") }
+        maven { url = uri("https://dl.bintray.com/jetbrains/intellij-third-party-dependencies/") }
+    }
+    dependencies {
+        classpath("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.15.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+group = "com.dbb"
+version = "1.8.2"
+
+
+repositories {
+    mavenLocal()
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2020.2")
+    type.set("IU") // Target IDE Platform
+    plugins.set(listOf("java"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+    patchPluginXml {
+        sinceBuild.set("222")
+        untilBuild.set("232.*")
+    }
+
+}
