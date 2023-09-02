cf27834fe864c08da36e91c5608a289b9abace1c
博麗靈夢 <1725338233@qq.com>
2023-09-02 09:21:29 +0800

    优化 FileToZipUtil

--- src/main/java/com/qbb/util/FileToZipUtil.java
+++ src/main/java/com/qbb/util/FileToZipUtil.java
@@ -1,16 +1,19 @@
 package com.qbb.util;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
- * @description: 文件压缩
- * @author: chengsheng@qbb6.com
- * @date: 2019/5/15
+ * 文件压缩
+ *
+ * @author chengsheng@qbb6.com
+ * @since 2019/5/15
  */
 public class FileToZipUtil {
 
@@ -24,35 +27,16 @@ public class FileToZipUtil {
      *                         false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
      * @throws RuntimeException 压缩失败会抛出运行时异常
      */
-    public static void toZip(Set<String> srcDir, String outDir,
-                             boolean KeepDirStructure) throws Exception {
-
-        OutputStream out = new FileOutputStream(new File(outDir));
-
+    public static void toZip(Set<String> srcDir, String outDir, boolean KeepDirStructure) {
         long start = System.currentTimeMillis();
-        ZipOutputStream zos = null;
-        try {
-            zos = new ZipOutputStream(out);
-            List<File> sourceFileList = new ArrayList<File>();
-            for (String dir : srcDir) {
-                File sourceFile = new File(dir);
-                sourceFileList.add(sourceFile);
-            }
+        try (ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(new File(outDir).toPath()))) {
+            List<File> sourceFileList = srcDir.stream().map(File::new).collect(Collectors.toList());
             compress(sourceFileList, zos, KeepDirStructure);
             long end = System.currentTimeMillis();
             System.out.println("压缩完成，耗时：" + (end - start) + " ms");
         } catch (Exception e) {
             throw new RuntimeException("zip error from ZipUtils", e);
-        } finally {
-            if (zos != null) {
-                try {
-                    zos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
     }
 
     /**
@@ -61,13 +45,11 @@ public class FileToZipUtil {
      * @param sourceFile       源文件
      * @param zos              zip输出流
      * @param name             压缩后的名称
-     * @param KeepDirStructure 是否保留原来的目录结构,
+     * @param keepDirStructure 是否保留原来的目录结构,
      *                         true:保留目录结构;
      *                         false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
-     * @throws Exception
      */
-    private static void compress(File sourceFile, ZipOutputStream zos,
-                                 String name, boolean KeepDirStructure) throws Exception {
+    private static void compress(File sourceFile, ZipOutputStream zos, String name, boolean keepDirStructure) throws Exception {
         byte[] buf = new byte[BUFFER_SIZE];
         if (sourceFile.isFile()) {
             zos.putNextEntry(new ZipEntry(name));
@@ -82,60 +64,26 @@ public class FileToZipUtil {
         } else {
             File[] listFiles = sourceFile.listFiles();
             if (listFiles == null || listFiles.length == 0) {
-                if (KeepDirStructure) {
+                if (keepDirStructure) {
                     zos.putNextEntry(new ZipEntry(name + "/"));
                     zos.closeEntry();
                 }
-
             } else {
                 for (File file : listFiles) {
-                    if (KeepDirStructure) {
-                        compress(file, zos, name + "/" + file.getName(),
-                                KeepDirStructure);
+                    if (keepDirStructure) {
+                        compress(file, zos, name + "/" + file.getName(), true);
                     } else {
-                        compress(file, zos, file.getName(), KeepDirStructure);
+                        compress(file, zos, file.getName(), false);
                     }
-
                 }
             }
         }
     }
 
-    private static void compress(List<File> sourceFileList,
-                                 ZipOutputStream zos, boolean KeepDirStructure) throws Exception {
-        byte[] buf = new byte[BUFFER_SIZE];
+    private static void compress(List<File> sourceFileList, ZipOutputStream zos, boolean keepDirStructure) throws Exception {
         for (File sourceFile : sourceFileList) {
             String name = sourceFile.getName();
-            if (sourceFile.isFile()) {
-                zos.putNextEntry(new ZipEntry(name));
-                int len;
-                FileInputStream in = new FileInputStream(sourceFile);
-                while ((len = in.read(buf)) != -1) {
-                    zos.write(buf, 0, len);
-                }
-                zos.closeEntry();
-                in.close();
-            } else {
-                File[] listFiles = sourceFile.listFiles();
-                if (listFiles == null || listFiles.length == 0) {
-                    if (KeepDirStructure) {
-                        zos.putNextEntry(new ZipEntry(name + "/"));
-                        zos.closeEntry();
-                    }
-
-                } else {
-                    for (File file : listFiles) {
-                        if (KeepDirStructure) {
-                            compress(file, zos, name + "/" + file.getName(),
-                                    KeepDirStructure);
-                        } else {
-                            compress(file, zos, file.getName(),
-                                    KeepDirStructure);
-                        }
-
-                    }
-                }
-            }
+            compress(sourceFile, zos, name, keepDirStructure);
         }
     }
 }
